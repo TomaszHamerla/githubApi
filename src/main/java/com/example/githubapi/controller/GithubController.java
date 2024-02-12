ac25d1908410d6d2fdd1ee64bdef7ef632fb9d60
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-02-12 14:15:51 +0100

    updated GithubController

--- src/main/java/com/example/githubapi/controller/GithubController.java
+++ src/main/java/com/example/githubapi/controller/GithubController.java
@@ -1,15 +1,24 @@
 package com.example.githubapi.controller;
 
+import com.example.githubapi.model.RepositoryInfo;
+import com.example.githubapi.service.GithubService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/repositories")
+@RequiredArgsConstructor
 public class GithubController {
-    @GetMapping
-    ResponseEntity<String>listUserRepositories(){
-        return ResponseEntity.ok("repositories");
+    private final GithubService githubService;
+
+    @GetMapping("/{username}")
+    ResponseEntity<List<RepositoryInfo>> listUserRepositories(@PathVariable String username) {
+        return ResponseEntity.ok(githubService.getUserRepositories(username));
     }
 }
