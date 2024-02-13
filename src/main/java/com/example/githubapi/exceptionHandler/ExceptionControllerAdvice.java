8c5ea96594d9a17db53f9d3c408065f562283461
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-02-13 19:43:04 +0100

    removed responseEntity

--- src/main/java/com/example/githubapi/exceptionHandler/ExceptionControllerAdvice.java
+++ src/main/java/com/example/githubapi/exceptionHandler/ExceptionControllerAdvice.java
@@ -2,7 +2,6 @@ package com.example.githubapi.exceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
@@ -12,14 +11,11 @@ import org.springframework.web.client.HttpClientErrorException;
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(HttpClientErrorException.NotFound.class)
-    public ResponseEntity<ErrorResponse> handleException(HttpClientErrorException e) {
-        ErrorResponse errorResponse = ErrorResponse.builder().
-                status(HttpStatus.NOT_FOUND.value()).
-                message("User with given name do not exists !").
-                build();
+    public ErrorResponse handleException(HttpClientErrorException e) {
+        String message = ("User with given name do not exists !");
+        ErrorResponse errorResponse = new ErrorResponse(404, message);
         log.error("User with given name do not exists !");
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(errorResponse);
+        return errorResponse;
     }
 
 }
